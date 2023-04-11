e748133dece665f5403048aa642f6f726ceab99d
BadeevIlya <70643384+BadeevIlya@users.noreply.github.com>
2023-04-11 16:28:19 +0300

    Change file import

--- data.py
+++ data.py
@@ -14,10 +14,9 @@ from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 import sys
 
-print(os.getcwd(), sys.path)
-if os.getcwd() not in sys.path:
-	print("CHECK____")
-	sys.path.append(os.getcwd())
+if 'kaggle' in os.getcwd():
+	print("CHECK______")
+	sys.path.append('/kaggle/working/OilTankVolumeEstimation/')
 	print(sys.path)
 
 import albumentations as A
