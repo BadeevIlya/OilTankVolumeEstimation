510293fc3f133dc51ea3578c79ca38374833a986
BadeevIlya <70643384+BadeevIlya@users.noreply.github.com>
2023-04-27 19:07:11 +0300

    Some changes in structure

--- utils/path.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import os
-
-DATASET_PATH = Path(input("Enter dataset path:"))
-WORKING_PATH = Path(os.getcwd())
-test_path = Path(WORKING_PATH / 'test')
-train_path = Path(WORKING_PATH / 'train')
